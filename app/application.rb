34ab5001347aea14101975574306f78141fcb86a
Gabriel Valle <gvalle91@gmail.com>
2018-07-01 14:50:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.new()
+    time_dif = TimeDifference.between(time, 1200).in_hours
     resp.write "#{time}"
     resp.finish
   end
