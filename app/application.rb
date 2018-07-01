b84b3bfda7c3ad27657c0ce9e11447c715976097
Gabriel Valle <gvalle91@gmail.com>
2018-07-01 14:52:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.new()
-    time_dif = (time - 1200) / 1.hour
+    time_dif = (time - 1200)
     resp.write "time now is : #{time}"
     resp.write "time difference is #{time_dif}"
     resp.finish
